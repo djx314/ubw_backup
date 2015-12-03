762f9c2b6750bd2e7ac89578d420043a93779e33
djx314 <djx314@sina.cn>
2015-12-03 16:56:19 +0800

    第三个版本。

--- ubw-core/src/test/scala/org/xarcher/ubw/hmap/SelectTest.scala
+++ ubw-core/src/test/scala/org/xarcher/ubw/hmap/SelectTest.scala
@@ -7,20 +7,21 @@ import org.scalatest._
 import org.scalatest.concurrent.PatienceConfiguration.Timeout
 import org.scalatest.concurrent._
 import org.scalatest.time.{Millis, Span}
-import slick.ast._
-import slick.jdbc.JdbcType
+import slick.ast.TypedType
+import slick.lifted.{CanBeQueryCondition, Ordered}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.language.higherKinds
 import scala.language.implicitConversions
 import slick.driver.H2Driver.api._
-import slick.lifted.{AbstractTable, Ordered, CanBeQueryCondition}
 
 import scala.reflect.ClassTag
 
 /**
   * Created by djx314 on 15-6-22.
   */
+
+case class RepDefaultValue[T](value: T)
 
 trait SlickData {
 
@@ -55,13 +56,13 @@ class PermissionTable(tag: slick.driver.H2Driver.api.Tag) extends Table[Permissi
 
 case class Cat(
   id: Option[Long] = None,
-  miao: Long = 0,
+  miao: Option[Long] = None,
   wang: String = ""
 )
 
 class CatTable(tag: slick.driver.H2Driver.api.Tag) extends Table[Cat](tag, "S_CAT") {
   def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
-  def miao = column[Long]("MIAO")
+  def miao = column[Option[Long]]("MIAO")
   def wang = column[String]("WANG")
 
   def * = (id.?, miao, wang) <> (Cat.tupled, Cat.unapply _)
@@ -73,6 +74,10 @@ with Matchers
 with BeforeAndAfter
 with OneInstancePerTest {
 
+  implicit object defaultRepStringValue extends RepDefaultValue("喵了个咪，真想日死 szeiger 偏要让我用这种旁门左道的方法")
+  implicit object defaultRepIntValue extends RepDefaultValue(Int.MinValue)
+  implicit object defaultRepLongValue extends RepDefaultValue(Long.MinValue)
+
   implicit def slickDataColumnType[T : BaseColumnType]: BaseColumnType[SlickDataGen[T]] =
     MappedColumnType.base[SlickDataGen[T], T](
       dt => dt.data,
@@ -81,50 +86,32 @@ with OneInstancePerTest {
 
   import slick.driver.H2Driver._
 
-  implicit def ggdgjhigherugihuriehg[T](implicit jdbcType: ColumnType[T]): ColumnType[SlickDataGen[Option[T]]] = {
-    class AA/*(implicit override val classTag: ClassTag[SlickDataGen[Option[T]]])*/extends MappedJdbcType[SlickDataGen[Option[T]], T]()(jdbcType, ClassTag[SlickDataGen[Option[T]]](classOf[SlickDataGen[Option[T]]]))/*TypedType[SlickDataGen[Option[T]]] with ScalaType[SlickDataGen[Option[T]]] with AtomicType*/{
-      /*def nullable: Boolean = true//jdbcType.scalaType.nullable
-      def ordered: Boolean = jdbcType.optionType.scalaType.ordered
-      def scalaOrderingFor(ord: Ordering): scala.math.Ordering[SlickDataGen[Option[T]]] = new scala.math.Ordering[SlickDataGen[Option[T]]] {
-        val uOrdering = jdbcType.optionType.scalaType.scalaOrderingFor(ord)//baseType.scalaType.scalaOrderingFor(ord)
-        def compare(x: SlickDataGen[Option[T]], y: SlickDataGen[Option[T]]): Int = uOrdering.compare(x.data, y.data)
-      }*/
-      println("22" * 100)
+  implicit def ggdgjhigherugihuriehg[T](implicit jdbcType: ColumnType[T], defaultValue: RepDefaultValue[T]): ColumnType[SlickDataGen[Option[T]]] = {
+    class AA extends MappedJdbcType[SlickDataGen[Option[T]], T]()(jdbcType, ClassTag[SlickDataGen[Option[T]]](classOf[SlickDataGen[Option[T]]])) {
       override def map(t: SlickDataGen[Option[T]]): T = t.data match {
         case Some(s) => s
-        case None => ???
+        case None => null.asInstanceOf[T]
       }
       override def comap(u: T): SlickDataGen[Option[T]] = {
-        SlickDataGen(Option(u))
+        if (u == defaultValue.value)
+          SlickDataGen(None)
+        else
+          SlickDataGen(Option(u))
+      }
+      override def getValue(r: ResultSet, idx: Int) = {
+        val v = tmd.getValue(r, idx)
+        val baseValue = if((v.asInstanceOf[AnyRef] eq null) || tmd.wasNull(r, idx))
+          defaultValue.value
+        else
+          v
+        comap(baseValue)
       }
       override def wasNull(r: ResultSet, idx: Int) = {
-        println("11" * 100)
-        println(r)
-        println(r.wasNull())
         r.wasNull()
-        false
       }
     }
     new AA()
   }
-
-  /*implicit def slickDataColumnOptionType[T](implicit typedType: JdbcType[T]): BaseColumnType[SlickDataGen[Option[T]]] = {
-    import slick.driver.H2Driver._
-
-    class MappedColumnType11(toBase:  => U, toMapped: U => S)(implicit override val classTag: ClassTag[S], val jdbcType: JdbcType[U]) extends TypedType[S] with ScalaType[S] with AtomicType/*ScalaType[T] with BaseTypedType[T]*/{
-      def nullable: Boolean = true//jdbcType.scalaType.nullable
-      def ordered: Boolean = jdbcType.scalaType.ordered
-      def scalaOrderingFor(ord: Ordering): scala.math.Ordering[S] = new scala.math.Ordering[S] {
-        val uOrdering = jdbcType.scalaType.scalaOrderingFor(ord)
-        def compare(x: S, y: S): Int = uOrdering.compare(toBase(x), toBase(y))
-      }
-      def comap(u: U): S = toMapped(u)
-      def map(t: S): U = toBase(t)
-    }
-
-    //new MappedColumnType11[SlickDataGen[Option[T]], T](dt => throw new SlickException("SlickDataGen 这个类只提供读的功能，写的功能暂不提供")/*dt.data.get*/, ts => SlickDataGen(Option(ts)))
-    ???
-  }*/
 
   implicit class columnTypeToSlickDataColumnType[T](baseRep: Rep[T])(implicit typedType: TypedType[SlickDataGen[T]]) {
     def miaolegemi = {
@@ -147,11 +134,11 @@ with OneInstancePerTest {
   before {
     val aa = permissionTq1 += Permission(
       name = "bb",
-      typeName = None,//Option("bb"),
+      typeName = /*None,*/Option("bb"),
       describe = "cc"
     )
     val aabb = catTq1 += Cat(
-      miao = 2345L,
+      //miao = 2345L,
       wang = "bb"
     )
     try {
@@ -207,24 +194,6 @@ with OneInstancePerTest {
 
     }
 
-    /*case class SqlFilter[S, R <: Rep[_] : CanBeQueryCondition](f: S => R) extends SqlFilterAbs {
-
-      override val convert = f
-      override type TableType = S
-      override type ResultType = R
-      override val wt = implicitly[CanBeQueryCondition[ResultType]]
-      /*@inline def myFilter[U, A[_]](query: Query[S, U, A]): Query[S, U, A] = {
-        query.filter(table1 => f(table1))(wt)
-      }*/
-
-    }*/
-    /*case class SqlOrder[S, R](f: S => R)(implicit val wt: R => Ordered) {
-
-      @inline def myOrder[U, A[_]](query: Query[S, U, A]): Query[S, U, A] = {
-        query.sortBy(table1 => wt(f(table1)))
-      }
-
-    }*/
     case class SqlSelect[S, T, R, G](f: S => R)(implicit val shape: Shape[_ <: FlatShapeLevel, R, T, G])
 
     case class SqlWrapper[S, T, U, G](
@@ -291,14 +260,9 @@ with OneInstancePerTest {
     db.run(query).map(println).futureValue(oneSecondTimeOut)
 
     def cc = {
-      val aa = implicitly[ColumnType[SlickDataGen[Option[String]]]]
-      val bb = ggdgjhigherugihuriehg[String]
       select(
         (table1: (PermissionTable, CatTable)) => {
-          (table1._1.name.miaolegemi, {
-            //println(table1._1.typeName.getClass)
-            table1._1.typeName.miaolegemi
-          }, table1._2.id.miaolegemi)
+          (table1._1.name.miaolegemi, table1._1.typeName.miaolegemi, table1._2.id.miaolegemi, table1._2.miao.miaolegemi)
         }
       )
       .where { case (table1, table2) => table1.describe === "cc" }
@@ -327,7 +291,8 @@ with OneInstancePerTest {
     val query1 = ccdd.aabb(permissionTq1, catTq1).result
 
     try {
-      db.run(query1).map(s => println("输出：" + s)).futureValue(oneSecondTimeOut)
+      db.run(query1).map(s => println("输出：" + s.head)).futureValue(oneSecondTimeOut)
+      db.run(permissionTq1.map(_.typeName.miaolegemi).result).map(s => println("输出1111：" + s.head)).futureValue(oneSecondTimeOut)
     } catch {
       case e: Exception => e.printStackTrace
     }
