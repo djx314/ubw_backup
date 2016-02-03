5e4c0bb3e2f95eb3b8034f003bbba5926ae79bcb
djx314 <djx314@sina.cn>
2016-02-03 17:10:06 +0800

    add boolean helper.

--- src/main/scala/net/scalax/ubw/helper/BooleanRepHelper.scala
+++ src/main/scala/net/scalax/ubw/helper/BooleanRepHelper.scala
@@ -7,7 +7,7 @@ import slick.lifted._
 case class RichBRep[P](rep: Option[Rep[P]]) {
 
   def &&&[P2, R](rRep2: RichBRep[P2])
-                (implicit om: OptionMapperDSL.arg[Boolean, P]#arg[Boolean, P2]#to[Boolean, R], convertP: Rep[P] => Rep[R], convertP2: Rep[P2] => Rep[R]): RichBRep[R] = {
+  (implicit om: OptionMapperDSL.arg[Boolean, P]#arg[Boolean, P2]#to[Boolean, R], convertP: Rep[P] => Rep[R], convertP2: Rep[P2] => Rep[R]): RichBRep[R] = {
     (rep -> rRep2.rep) match {
       case (Some(subRep1), Some(subRep2)) =>
         RichBRep(Option(new BooleanColumnExtensionMethods(subRep1).&&(subRep2)(om)))
@@ -21,7 +21,7 @@ case class RichBRep[P](rep: Option[Rep[P]]) {
   }
 
   def |||[P2, R](rRep2: RichBRep[P2])
-                (implicit om: OptionMapperDSL.arg[Boolean, P]#arg[Boolean, P2]#to[Boolean, R], convertP: Rep[P] => Rep[R], convertP2: Rep[P2] => Rep[R]): RichBRep[R] = {
+    (implicit om: OptionMapperDSL.arg[Boolean, P]#arg[Boolean, P2]#to[Boolean, R], convertP: Rep[P] => Rep[R], convertP2: Rep[P2] => Rep[R]): RichBRep[R] = {
     (rep -> rRep2.rep) match {
       case (Some(subRep1), Some(subRep2)) =>
         RichBRep(Option(new BooleanColumnExtensionMethods(subRep1).||(subRep2)(om)))
@@ -35,7 +35,7 @@ case class RichBRep[P](rep: Option[Rep[P]]) {
   }
 
   def &&&[P2, R](when: Boolean, rRep2: => Rep[P2])
-                (implicit om: OptionMapperDSL.arg[Boolean, P]#arg[Boolean, P2]#to[Boolean, R], convertP: Rep[P] => Rep[R], convertP2: Rep[P2] => Rep[R]): RichBRep[R] = {
+    (implicit om: OptionMapperDSL.arg[Boolean, P]#arg[Boolean, P2]#to[Boolean, R], convertP: Rep[P] => Rep[R], convertP2: Rep[P2] => Rep[R]): RichBRep[R] = {
     if (when) {
       &&&(RichBRep(Option(rRep2)))
     } else {
@@ -44,7 +44,7 @@ case class RichBRep[P](rep: Option[Rep[P]]) {
   }
 
   def |||[P2, R](when: Boolean, rRep2: => Rep[P2])
-                (implicit om: OptionMapperDSL.arg[Boolean, P]#arg[Boolean, P2]#to[Boolean, R], convertP: Rep[P] => Rep[R], convertP2: Rep[P2] => Rep[R]): RichBRep[R] = {
+    (implicit om: OptionMapperDSL.arg[Boolean, P]#arg[Boolean, P2]#to[Boolean, R], convertP: Rep[P] => Rep[R], convertP2: Rep[P2] => Rep[R]): RichBRep[R] = {
     if (when) {
       |||(RichBRep(Option(rRep2)))
     } else {
@@ -53,7 +53,7 @@ case class RichBRep[P](rep: Option[Rep[P]]) {
   }
 
   def &&&[P2, R](rRep2: Rep[P2])
-                (implicit om: OptionMapperDSL.arg[Boolean, P]#arg[Boolean, P2]#to[Boolean, R], convertP: Rep[P] => Rep[R], convertP2: Rep[P2] => Rep[R]): RichBRep[R] = {
+    (implicit om: OptionMapperDSL.arg[Boolean, P]#arg[Boolean, P2]#to[Boolean, R], convertP: Rep[P] => Rep[R], convertP2: Rep[P2] => Rep[R]): RichBRep[R] = {
     &&&(RichBRep(Option(rRep2)))
   }
 
@@ -79,83 +79,15 @@ object RichBRep {
 
 trait BooleanRepHelper {
 
-  implicit def booleanRepToRichRep[T](rep: Rep[T]): RichBRep[T] = RichBRep(Option(rep))
-  implicit def booleanNeedRepToRichRep[T](plus: (Boolean, Rep[T])): RichBRep[T] = if (plus._1) {
+  @inline implicit def booleanRepToRichRep[T](rep: Rep[T]): RichBRep[T] = RichBRep(Option(rep))
+
+  @inline implicit def booleanNeedRepToRichRep[T](plus: (Boolean, Rep[T])): RichBRep[T] = if (plus._1) {
     RichBRep(Option(plus._2))
   } else {
     RichBRep.empty[T]
   }
-  implicit def booleanToOptionBooleanRep[T](baseRep: Rep[Boolean])(implicit columnMethods: Rep[Boolean] => BaseColumnExtensionMethods[Boolean]): Rep[Option[Boolean]] = columnMethods(baseRep).?
 
-  /*implicit class FilterIfNeedHelper[P1](rep1: Rep[P1]) {
-
-    @inline def &&&[P2, R](need: Boolean, rep2: Rep[P2])
-      (implicit om: OptionMapperDSL.arg[Boolean, P1]#arg[Boolean, P2]#to[Boolean, R], convert: Rep[P1] => Rep[R]): Rep[R] = {
-      if (need)
-        new BooleanColumnExtensionMethods(rep1).&&(rep2)
-      else
-        convert(rep1)
-    }
-
-    @inline def |||[P2, R](need: Boolean, rep2: Rep[P2])
-      (implicit om: OptionMapperDSL.arg[Boolean, P1]#arg[Boolean, P2]#to[Boolean, R], convert: Rep[P1] => Rep[R]): Rep[R] =
-      if (need)
-        new BooleanColumnExtensionMethods(rep1).||(rep2)
-      else
-        convert(rep1)
-
-    @inline def &&&[P2, R](rep2: Rep[P2])
-                          (implicit om: OptionMapperDSL.arg[Boolean, P1]#arg[Boolean, P2]#to[Boolean, R]): Rep[R] = {
-      new BooleanColumnExtensionMethods(rep1).&&(rep2)
-    }
-
-    @inline def |||[P2, R](rep2: Rep[P2])
-                          (implicit om: OptionMapperDSL.arg[Boolean, P1]#arg[Boolean, P2]#to[Boolean, R]): Rep[R] =
-      new BooleanColumnExtensionMethods(rep1).||(rep2)
-
-  }
-
-  implicit class FilterIfNeedRepHelper[P1](repContent1: (Boolean, Rep[P1])) {
-
-    @inline def &&&[P2, R](rep2: Rep[P2])
-      (implicit om: OptionMapperDSL.arg[Boolean, P1]#arg[Boolean, P2]#to[Boolean, R], convert: Rep[R] => Rep[P2]): Rep[P2] = {
-      val (need, rep1) = repContent1
-      if (need)
-        convert(new BooleanColumnExtensionMethods(rep1).&&(rep2))
-      else
-        rep2
-    }
-
-    @inline def |||[P2, R](rep2: Rep[P2])
-      (implicit om: OptionMapperDSL.arg[Boolean, P1]#arg[Boolean, P2]#to[Boolean, R], convert: Rep[R] => Rep[P2]): Rep[P2] = {
-      val (need, rep1) = repContent1
-      if (need)
-        convert(new BooleanColumnExtensionMethods(rep1).||(rep2))
-      else
-        rep2
-    }
-
-  }
-
-  trait NeedFilter {
-
-    def &&[P1, P2, R](need: Boolean, rep1: Rep[P1], rep2: Rep[P2])
-    (implicit om: OptionMapperDSL.arg[Boolean, P1]#arg[Boolean, P2]#to[Boolean, R])
-    : Rep[R] = {
-      new BooleanColumnExtensionMethods(rep1).&&(rep2)
-    }
-
-    def ||[P1, P2, R](need: Boolean, rep1: Rep[P1], rep2: Rep[P2])
-    (implicit om: OptionMapperDSL.arg[Boolean, P1]#arg[Boolean, P2]#to[Boolean, R])
-    : Rep[R] = {
-      new BooleanColumnExtensionMethods(rep1).||(rep2)
-    }
-
-    def unary_![P1](need: Boolean, rep1: Rep[P1]): Rep[P1] = {
-      new BooleanColumnExtensionMethods(rep1).unary_!
-    }
-
-  }*/
+  @inline implicit def booleanToOptionBooleanRep(baseRep: Rep[Boolean])(implicit columnMethods: Rep[Boolean] => BaseColumnExtensionMethods[Boolean]): Rep[Option[Boolean]] = columnMethods(baseRep).?
 
 }
 
